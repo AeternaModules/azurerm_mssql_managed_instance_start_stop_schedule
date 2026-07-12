75b31daa9b31d091b425065710dca5b69f178f9b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "mssql_managed_instance_start_stop_schedules" {
-  description = "All mssql_managed_instance_start_stop_schedule resources"
-  value       = azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules
-}
 output "mssql_managed_instance_start_stop_schedules_description" {
-  description = "List of description values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.description]
+  description = "Map of description values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.description }
 }
 output "mssql_managed_instance_start_stop_schedules_managed_instance_id" {
-  description = "List of managed_instance_id values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.managed_instance_id]
+  description = "Map of managed_instance_id values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.managed_instance_id }
 }
 output "mssql_managed_instance_start_stop_schedules_next_execution_time" {
-  description = "List of next_execution_time values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.next_execution_time]
+  description = "Map of next_execution_time values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.next_execution_time }
 }
 output "mssql_managed_instance_start_stop_schedules_next_run_action" {
-  description = "List of next_run_action values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.next_run_action]
+  description = "Map of next_run_action values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.next_run_action }
 }
 output "mssql_managed_instance_start_stop_schedules_schedule" {
-  description = "List of schedule values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.schedule]
+  description = "Map of schedule values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.schedule }
 }
 output "mssql_managed_instance_start_stop_schedules_timezone_id" {
-  description = "List of timezone_id values across all mssql_managed_instance_start_stop_schedules"
-  value       = [for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : v.timezone_id]
+  description = "Map of timezone_id values across all mssql_managed_instance_start_stop_schedules, keyed the same as var.mssql_managed_instance_start_stop_schedules"
+  value       = { for k, v in azurerm_mssql_managed_instance_start_stop_schedule.mssql_managed_instance_start_stop_schedules : k => v.timezone_id }
 }
 
